f723d5fe427497c8a28f096c3c4f7fb74923ad74
kuwatxkat <kuwatxkat@gmail.com>
2020-07-19 16:25:25 -0700

    Committing buggy sprite code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
-#include "SFML/Window.hpp"
+#include <iostream>
+
 #include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  window.clear();
+
+  sf::Texture texture;
+  if (!texture.loadFromFile("../resources/Mario & Luigi.png", sf::IntRect(34, 80, 16, 16)))
+  {
+      std::cerr << "Error Loading Texture";
+  }
+  sf::Sprite sprite;
+  sprite.setTexture(texture);
+  sprite.setPosition(sf::Vector2f(10.f, 50.f);
+  window.draw(sprite);
+
+
 
   while (window.isOpen())
   {
     sf::Event event = {};
     while (window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
-        window.close();
+        if (event.type == sf::Event::Closed)
+            window.close();
     }
-
-    window.clear();
-    window.draw(shape);
     window.display();
   }
 
