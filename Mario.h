bc8f57265d2855e9b3a462f5bd90c05f9f6fb3d2
JETM319 <jetmeans@gmail.com>
2020-07-26 19:09:52 -0400

    Add walking and jumping animations

---
+++ Mario.h
@@ -0,0 +1,41 @@
+#ifndef SUPERMARIOBROS_MARIO_H
+#define SUPERMARIOBROS_MARIO_H
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+class Mario
+{
+public:
+    Mario();
+    void draw(sf::RenderWindow& window);
+
+    void walk();
+
+    void setPosition(size_t x, size_t y);
+    size_t getX() const;
+    size_t getY() const;
+
+    void stopWalking();
+
+    void jump();
+
+private:
+    sf::Texture mTexture;
+
+    sf::Sprite* mActiveSprite{};
+
+    // Walking animation
+    std::vector<sf::Sprite> mWalkingSprites;
+    size_t mSpriteIndex;
+    size_t mTicksPerFrame;
+    size_t mRemainingTicsThisFrame;
+
+    // Jumping animation
+    std::vector<sf::Sprite> mJumpingSprites;
+    size_t mJumpingIndex;
+    size_t mJumpingTicksPerFrame;
+    size_t mJumpingRemainingTicsThisFrame;
+};
+
+#endif  // SUPERMARIOBROS_MARIO_H
--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
-#include "mario.h"
+#include "Mario.h"
 
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "Super Mario Bros");
-  window.clear();
-  mario sprite;
-  sprite.draw(window);
-  window.display();
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Super Mario Bros");
+    window.setFramerateLimit(30);
+    window.clear();
+    Mario sprite;
+    sprite.draw(window);
+    window.display();
 
-  while (window.isOpen())
-  {
-    sf::Event event = {};
-    while (window.pollEvent(event))
+    while (window.isOpen())
     {
-        if (event.type == sf::Event::Closed)
-            window.close();
-    }
-  }
+        sf::Event event = {};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-  return 0;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            sprite.jump();
+        }
+        else
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                sprite.setPosition(sprite.getX() + 1, sprite.getY());
+                sprite.walk();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                sprite.setPosition(sprite.getX() - 1, sprite.getY());
+                sprite.walk();
+            }
+            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right) &&
+                !sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                sprite.stopWalking();
+        }
+
+        window.clear();
+        sprite.draw(window);
+        window.display();
+    }
+
+    return 0;
 }
