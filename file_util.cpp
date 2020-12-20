6af8b72fbfa4889ef49a71bbce2b3c7ba5f37ac5
kuwatxkat <kuwatxkat@gmail.com>
2020-12-20 15:52:01 -0800

    Set up entity collision test file and modified findRootDirectory method

--- file_util.cpp
+++ file_util.cpp
@@ -1,12 +1,21 @@
 #include <file_util.h>
+#include <filesystem>
 
 std::string findRootDirectory(const std::string& argv0)
 {
-    const auto lastSlash = argv0.rfind('/');
-    if (lastSlash == std::string::npos)
-        return "..";
-    const auto secondLastSlash = argv0.rfind('/', lastSlash - 1);
-    if (secondLastSlash == std::string::npos)
-        return ".";
-    return argv0.substr(0, secondLastSlash + 1);
+//    const auto lastSlash = argv0.rfind('/');
+//    if (lastSlash == std::string::npos)
+//        return "..";
+//    const auto secondLastSlash = argv0.rfind('/', lastSlash - 1);
+//    if (secondLastSlash == std::string::npos)
+//        return ".";
+//    return argv0.substr(0, secondLastSlash + 1);
+    auto lastSlash = argv0.rfind('/');
+    auto directory = argv0.substr(0, lastSlash + 1);
+    while (!std::filesystem::exists(directory + "/resources"))
+    {
+        lastSlash = argv0.rfind('/', lastSlash - 1);
+        directory = argv0.substr(0, lastSlash + 1);
+    }
+    return directory;
 }
