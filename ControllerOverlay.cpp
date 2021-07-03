8da6cd0af09d2d7a9c3fb4bebd8de9d511d4500e
JETM319 <jetmeans@gmail.com>
2021-07-03 18:50:48 -0400

    Ensure contoller graphic doesn't react to scrolling

--- ControllerOverlay.cpp
+++ ControllerOverlay.cpp
@@ -6,10 +6,12 @@
 void ControllerOverlay::draw(const KeyboardInput& keyboardInput, sf::RenderWindow& window)
 {
     // Keypad
+    const auto view = window.getView();
+    const auto leftOffset = view.getCenter().x - (view.getSize().x / 2);
     sf::RectangleShape left(sf::Vector2f(10, 5));
     sf::RectangleShape right(sf::Vector2f(10, 5));
     sf::RectangleShape up(sf::Vector2f(5, 10));
-    sf::RectangleShape down(sf::Vector2f(5, 10));
+    sf::RectangleShape down(sf::Vector2f( 5, 10));
 
     sf::Color red = sf::Color(150, 0, 0);
     sf::Color green = sf::Color(0, 150, 0);
@@ -20,10 +22,10 @@ void ControllerOverlay::draw(const KeyboardInput& keyboardInput, sf::RenderWindo
     down.setFillColor(keyboardInput.down.keyIsDown ? green : red);
 
 
-    left.setPosition(120, 180);
-    right.setPosition(135, 180);
-    up.setPosition(130, 170);
-    down.setPosition(130, 185);
+    left.setPosition(leftOffset + 120, 180);
+    right.setPosition(leftOffset + 135, 180);
+    up.setPosition(leftOffset + 130, 170);
+    down.setPosition(leftOffset + 130, 185);
 
     window.draw(left);
     window.draw(right);
@@ -37,8 +39,8 @@ void ControllerOverlay::draw(const KeyboardInput& keyboardInput, sf::RenderWindo
     bButton.setFillColor(keyboardInput.B.keyIsDown ? green : red);
     aButton.setFillColor(keyboardInput.A.keyIsDown ? green : red);
 
-    bButton.setPosition(150, 178);
-    aButton.setPosition(165, 178);
+    bButton.setPosition(leftOffset + 150, 178);
+    aButton.setPosition(leftOffset + 165, 178);
 
     window.draw(bButton);
     window.draw(aButton);
